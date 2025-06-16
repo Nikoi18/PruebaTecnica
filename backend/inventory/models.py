0fc025f1c90985499f5f67af4b08865826f34159
Nicolas A. Urbaez A. <aran.nick15@gmail.com>
2025-06-16 20:51:23 +0200

    feat(api):Se impplementa Endpoint inventarios y movimientos

--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class InventoryItem(models.Model):
+    sku = models.CharField(max_length=100, unique=True, verbose_name='SKU')
+    ean13 = models.CharField(max_length=13, unique=True, verbose_name='EAN13')
+    quantity = models.IntegerField(verbose_name='Cantidad de Stock')
+
+    def __str__(self):
+        return f"{self.sku}({self.quantity})"
+    
+class StockMovement(models.Model):
+    item = models.ForeignKey(InventoryItem, on_delete=models.CASCADE, verbose_name='Item o movimientos')
+    quantity_change = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    reason = models.CharField(max_length=255, default="Actualizacion manual")
+
+    def __str__(self):
+        return f"{self.item.sku} - Cambio {self.quantity_change} el {self.timestamp.strftime('%Y-%m-%d %H:%M')}"
+    
--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import InventoryItem, StockMovement
+from .serializers import InventoryItemSerializer, StockMovementSerializer
 
-# Create your views here.
+
+class InventoryList(generics.ListAPIView):
+    queryset = InventoryItem.objects.all()
+    serializer_class = InventoryItemSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class InventoryItemUpdate(generics.UpdateAPIView):
+    queryset = InventoryItem.objects.all()
+    serializer_class = InventoryItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        new_quantity = request.data.get('quantity')
+
+        if new_quantity is not None:
+            quantity_change = int(new_quantity) - instance.quantity
+
+            StockMovement.objects.create(
+                item=instance,
+                quantity_change=quantity_change,
+                reason=f"Actualización desde la App"            )
+
+            instance.quantity = new_quantity
+            instance.save()
+        
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+class StockMovementHistory(generics.ListAPIView):
+    queryset = StockMovement.objects.all().order_by('-timestamp')
+    serializer_class = StockMovementSerializer
+    permission_classes = [IsAuthenticated]
